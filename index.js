e75f1f626990edcc9cdb2ced2f3babdc9cb9bde9
Franco Bouly <fbouly@gmail.com>
2018-09-13 17:21:18 +0200

    Add cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Setup basic express server
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 const Socket = require('./src/socket').default
 
